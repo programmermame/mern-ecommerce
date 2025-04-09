24f00b8f2c878900a0fd33c0ab665f8690467782
programmermame <mameatpickout@gmail.com>
2025-04-09 11:49:57 +0300

    implement automatica calculation of totalAmount

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -1,19 +1,34 @@
 import Order from "../models/Order.js";
+import Product from "../models/Product.js";
 
 const createOrders = async (req, res) => {
-    const { user, products, totalAmount } = req.body;
+    const { user, products } = req.body;
     try {
-        const newOrder = await Order.create(
-            {
-                user,
-                products,
-                totalAmount
-            })
-        res.status(201).json({ message: newOrder });
+        let totalAmount = 0;
+
+
+        for (let i = 0; i < products.length; i++) {
+            const product = await Product.findById(products[i].productId);
+
+            if (product) {
+                totalAmount += product.price * products[i].quantity;
+            } else {
+                return res.status(404).json({ message: `Product with ID ${products[i].productId} not found.` });
+            }
+        }
+
+        const newOrder = await Order.create({
+            user,
+            products,
+            totalAmount
+        });
+
+        res.status(201).json({ message: "Order created successfully", order: newOrder });
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        res.status(500).json({ message: error.message });
     }
-}
+};
+
 const getUserOrders = async (req, res) => {
     const { userId } = req.params;
     try {
