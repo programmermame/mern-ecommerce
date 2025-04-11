2b12d0f9e86b984868ae6d1d9b2f6e8c5883f23b
programmermame <mameatpickout@gmail.com>
2025-04-11 16:34:34 +0300

    use process.env.PORT for Render deployment

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,9 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port:process.env.PORT || 5173
+    port: process.env.PORT || 5173,
     proxy: {
       '/api': 'http://localhost:3000',  // Proxy requests to backend
-    },}
+    },
+  }
 
 })
