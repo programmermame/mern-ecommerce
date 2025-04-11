7d3daa516d9b24ce2bcec58555246d5317325d53
programmermame <mameatpickout@gmail.com>
2025-04-11 16:51:36 +0300

    allow Render domain in allowedHosts

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 5173,
+    allowedHosts: ['mern-ecommerce-aeqr.onrender.com'],
     proxy: {
       '/api': 'http://localhost:3000',  // Proxy requests to backend
     },
