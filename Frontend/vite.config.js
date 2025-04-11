06607547692f0b9f0d9a0063957169df39543a76
programmermame <mameatpickout@gmail.com>
2025-04-11 16:42:48 +0300

    set  host to  '0.0.0.0'

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    host: '0.0.0.0',
     port: process.env.PORT || 5173,
     proxy: {
       '/api': 'http://localhost:3000',  // Proxy requests to backend
