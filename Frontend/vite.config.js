bfaad4263e86bb99ddd3066357b853d25cfc5877
programmermame <mameatpickout@gmail.com>
2025-04-12 09:35:17 +0300

    FIX

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,13 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: 'dist',
-  },
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 5173,
-    allowedHosts: ['*'],
+    allowedHosts: ['https://pickout.onrender.com'],
     proxy: {
       // '/api': 'http://localhost:3000',  // Proxy requests to backend
       '/api': 'https://mern-ecommerce-backend-hpp7.onrender.com',  // Proxy requests to backend
