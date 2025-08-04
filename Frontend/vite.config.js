3ce4ede4e9e8d8c55d6d1bd78c7700dc4a9603d1
programmermame <mameatpickout@gmail.com>
2025-08-04 13:25:10 +0300

    update hosting service

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,10 +8,9 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 5173,
-    allowedHosts: ['pick-out-backend-service-on-render.onrender.com'],
     proxy: {
       // '/api': 'http://localhost:3000',  // Proxy requests to backend
-      '/api': 'https://pick-out-backend-service-on-render.onrender.com/',  // Proxy requests to backend
+      '/api': 'https://pick-out-backend-service-on-render.onrender.com',  // Proxy requests to backend
     },
   }
 
